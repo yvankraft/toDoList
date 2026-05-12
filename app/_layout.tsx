681b984e660e250bcc9da23aac6804864512b163
yvan wildis ngone Tchinda <yvanngone53@gmail.com>
2026-05-12 14:26:16 +0200

    Refactor code structure for improved readability and maintainability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,12 +7,18 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "./global.css";
+import * as SplashScreen from "expo-splash-screen";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
